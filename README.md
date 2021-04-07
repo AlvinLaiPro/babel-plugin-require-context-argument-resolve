6843d85cda520f5d4b6189ee591a69502755c691
Patrick Lai <patrick.lai@ringcentral.com>
2021-04-07 13:19:02 +0800

    search dependency by string

--- README.md
+++ README.md
@@ -14,8 +14,17 @@ babelrc:
 ]
 
 ```
+
+or omit the option:
+
+```js
+"plugins": [
+    ["require-context-argument-resolve"]
+]
+```
+
 ## How it works
-This plugin will match the first string parameters that you pass to `require.context` with the key of this plugin options that provided by you and then replace it with the value of that specific key.
+This plugin will match the first string parameters that you pass to `require.context` with the key of this plugin options that provided by you and then replace it with the value of that specific key. Or you can omit the option and let this plugin try to resolve.
 
 ## Note
-You still need `babel-plugin-require-context-hook` to support `require.context` in the test environment.
+You still need `babel-plugin-require-context-hook` to support `require.context` in the test environment. And please put this plugin before `babel-plugin-require-context-hook`.
--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+const path = require('path');
+const ResolvePathByString = (str, filename) => {
+    if (str.startsWith('.') || str.startsWith('/')) {
+        return str;
+    }
+
+    try {
+        const options = filename.split(path.sep).reduce((acc, cur, i, arr) => {
+            const item = `${arr.slice(0, -(i + 1)).join(path.sep)}${path.sep}package.json`;
+            acc.push(item);
+            return acc;
+        }, []);
+
+        let resolvePath = '';
+
+        for (let pkgPath of options) {
+            try {
+                const { dependencies = {}, devDependencies = {} } = require(pkgPath);
+                const deps = { ...dependencies, ...devDependencies };
+                const depKey = Object.keys(deps).find((key) => str.startsWith(key));
+
+                if (depKey) {
+                    const depPath = require.resolve(depKey);
+                    const directories = path.dirname(depPath).split(path.sep);
+                    const endPoints = str.replace(`${depKey}${path.sep}`, '').split(path.sep);
+                    const firstEndPoint = endPoints[0];
+                    const index = directories.lastIndexOf(firstEndPoint);
+
+                    if (index > -1) {
+                        resolvePath = directories.slice(0, index).concat(endPoints).join(path.sep);
+                    } else {
+                        resolvePath = directories.concat(endPoints).join(path.sep);
+                    }
+
+                    break;
+                }
+            } catch (err) {
+                continue;
+            }
+        }
+
+        return resolvePath || str;
+    } catch (error) {
+        return str;
+    }
+};
 module.exports = function ({ types: t }) {
     return {
         visitor: {
-            CallExpression: (path, { opts }) => {
+            CallExpression: (nodePath, { opts = {}, filename }) => {
                 if (
-                    t.isMemberExpression(path.node.callee, { computed: false }) &&
-                    t.isIdentifier(path.get('callee').node.object, { name: 'require' }) &&
-                    t.isIdentifier(path.get('callee').node.property, { name: 'context' }) &&
-                    t.isStringLiteral(path.get('arguments')[0])
+                    t.isMemberExpression(nodePath.node.callee, { computed: false }) &&
+                    t.isIdentifier(nodePath.get('callee').node.object, { name: 'require' }) &&
+                    t.isIdentifier(nodePath.get('callee').node.property, { name: 'context' }) &&
+                    t.isStringLiteral(nodePath.get('arguments')[0])
                 ) {
-                    const args = path.node.arguments;
-                    const [{value}] = args;
+                    const args = nodePath.node.arguments;
+                    const [{ value }] = args;
 
-                    if (opts[value]) {
-                        args[0] = t.stringLiteral(opts[value]);
-                    }
+                    args[0] = opts[value]
+                        ? t.stringLiteral(opts[value])
+                        : t.stringLiteral(ResolvePathByString(value, filename));
                 }
             },
         },
